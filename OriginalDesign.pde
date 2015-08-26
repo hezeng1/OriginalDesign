12ff1d6beebb8d3543abbcecdd5772433af9d811
hezeng1 <herman.zeng114@gmail.com>
2015-08-26 11:39:36 -0700

    nyes

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -8,13 +8,20 @@ void draw()
 {
   background(20,30,40);
   shrekFace();
+  shrekEyes(); 
  }
 void shrekFace()
 {
    fill(159,251,0);
    ellipse(shrekX+250,shrekY+250,340,350);
    bezier(50,90,50,60,100,90,150,150);
+   bezier(350,150,390,150,360,100,450,90);
    
 }
-
+void shrekEyes()
+{
+   fill((int)(Math.random()*254),(int)(Math.random()*256),(int)(Math.random()*256));
+   ellipse(180,220,70,40);
+   ellipse(280,220,70,40);
+}
 
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -24,15 +24,22 @@ public void draw()
 {
   background(20,30,40);
   shrekFace();
+  shrekEyes(); 
  }
 public void shrekFace()
 {
    fill(159,251,0);
    ellipse(shrekX+250,shrekY+250,340,350);
    bezier(50,90,50,60,100,90,150,150);
+   bezier(350,150,390,150,360,100,450,90);
    
 }
-
+public void shrekEyes()
+{
+   fill((int)(Math.random()*254),(int)(Math.random()*256),(int)(Math.random()*256));
+   ellipse(180,220,70,40);
+   ellipse(280,220,70,40);
+}
 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
